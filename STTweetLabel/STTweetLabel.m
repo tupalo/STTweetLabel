ce0e185394dc6c808c9f65ee974298bebf95a65b
Clemens Beer <clemens.beer@gmail.com>
2015-06-16 17:35:49 +0200

    allow ‘-‘ in hotWords

--- STTweetLabel/STTweetLabel.m
+++ STTweetLabel/STTweetLabel.m
@@ -155,8 +155,8 @@
 
     // Define a character set for the complete world (determine the end of the hot word)
     NSMutableCharacterSet *validCharactersSet = [NSMutableCharacterSet alphanumericCharacterSet];
-    [validCharactersSet removeCharactersInString:@"!@#$%^&*()-={[]}|;:',<>.?/"];
-    [validCharactersSet addCharactersInString:@"_"];
+    [validCharactersSet removeCharactersInString:@"!@#$%^&*()={[]}|;:',<>.?/"];
+    [validCharactersSet addCharactersInString:@"_-"];
 
     _rangesOfHotWords = [[NSMutableArray alloc] init];
 
